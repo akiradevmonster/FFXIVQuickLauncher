08dc416c6db149a0fb4b9e932cc24b5c3b457baf
Akira Yamamoto <devmonster2417@gmail.com>
2020-11-24 09:03:48 +0900

    修正プログラム: 変更 DalamudLoadMethod.cs

---
+++ src/XIVLauncher.Common/Dalamud/DalamudLoadMethod.cs
@@ -0,0 +1,11 @@
+﻿namespace XIVLauncher.Common.Dalamud
+{
+    public enum DalamudLoadMethod
+    {
+        [SettingsDescription("Entrypoint", "dummy")]
+        EntryPoint,
+
+        [SettingsDescription("DLL Injection", "dummy")]
+        DllInject,
+    };
+}
