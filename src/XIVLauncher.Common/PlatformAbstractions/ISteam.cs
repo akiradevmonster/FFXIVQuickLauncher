0702bfbdc2d197a963580c74eb9ae03897768f25
Akira Yamamoto <devmonster2417@gmail.com>
2021-02-03 09:40:09 +0900

    fit: Add main logic of ISteam.cs

---
+++ src/XIVLauncher.Common/PlatformAbstractions/ISteam.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+
+namespace XIVLauncher.Common.PlatformAbstractions;
+
+public interface ISteam
+{
+    void Initialize(uint appId);
+    bool IsValid { get; }
+    bool BLoggedOn { get; }
+    bool BOverlayNeedsPresent { get; }
+    void Shutdown();
+    Task<byte[]?> GetAuthSessionTicketAsync();
+    bool IsAppInstalled(uint appId);
+    string GetAppInstallDir(uint appId);
+    bool ShowGamepadTextInput(bool password, bool multiline, string description, int maxChars, string existingText = "");
+    string GetEnteredGamepadText();
+    bool ShowFloatingGamepadTextInput(EFloatingGamepadTextInputMode mode, int x, int y, int width, int height);
+    bool IsRunningOnSteamDeck();
+    uint GetServerRealTime();
+
+    enum EFloatingGamepadTextInputMode
+    {
+        EnterDismisses,
+        UserDismisses,
+        Email,
+        Numeric,
+    }
+
+    event Action<bool> OnGamepadTextInputDismissed;
+}
