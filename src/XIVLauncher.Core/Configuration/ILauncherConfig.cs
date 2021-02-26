5c2f928db059e724243280f444f27c19df171825
Akira Yamamoto <devmonster2417@gmail.com>
2021-02-26 09:41:10 +0900

    Create main logic of ILauncherConfig.cs

---
+++ src/XIVLauncher.Core/Configuration/ILauncherConfig.cs
@@ -0,0 +1,75 @@
+using XIVLauncher.Common;
+using XIVLauncher.Common.Addon;
+using XIVLauncher.Common.Dalamud;
+using XIVLauncher.Common.Game.Patch.Acquisition;
+using XIVLauncher.Core.Compatibility;
+using XIVLauncher.Core.Configuration.Linux;
+
+namespace XIVLauncher.Core.Configuration;
+
+public interface ILauncherConfig
+{
+    public string? CurrentAccountId { get; set; }
+
+    public string? AcceptLanguage { get; set; }
+
+    public bool? IsAutologin { get; set; }
+
+    public DirectoryInfo? GamePath { get; set; }
+
+    public string? AdditionalArgs { get; set; }
+
+    public ClientLanguage? ClientLanguage { get; set; }
+
+    public bool? IsUidCacheEnabled { get; set; }
+
+    public float? GlobalScale { get; set; }
+
+    public DpiAwareness? DpiAwareness { get; set; }
+
+    public bool? TreatNonZeroExitCodeAsFailure { get; set; }
+
+    public List<AddonEntry>? Addons { get; set; }
+
+    public bool? IsDx11 { get; set; }
+
+    public bool? IsEncryptArgs { get; set; }
+
+    public bool? IsFt { get; set; }
+
+    public bool? IsOtpServer { get; set; }
+
+    #region Patching
+
+    public DirectoryInfo? PatchPath { get; set; }
+
+    public bool? KeepPatches { get; set; }
+
+    public AcquisitionMethod? PatchAcquisitionMethod { get; set; }
+
+    public long PatchSpeedLimit { get; set; }
+
+    #endregion
+
+    #region Linux
+
+    public LinuxStartupType? LinuxStartupType { get; set; }
+
+    public string? LinuxStartCommandLine { get; set; }
+
+    public Dxvk.DxvkHudType DxvkHudType { get; set; }
+
+    public string? WineDebugVars { get; set; }
+
+    #endregion
+
+    #region Dalamud
+
+    public bool? DalamudEnabled { get; set; }
+
+    public DalamudLoadMethod? DalamudLoadMethod { get; set; }
+
+    public int DalamudLoadDelay { get; set; }
+
+    #endregion
+}
