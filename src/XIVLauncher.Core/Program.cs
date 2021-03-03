2e08153ad48cef429d9ed7105e8424afb500eb91
Akira Yamamoto <devmonster2417@gmail.com>
2021-03-03 16:43:20 +0900

    Create main logic of Program.cs

---
+++ src/XIVLauncher.Core/Program.cs
@@ -0,0 +1,239 @@
+﻿using System.Numerics;
+using Config.Net;
+using ImGuiNET;
+using XIVLauncher.Core.Style;
+using Serilog;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+using XIVLauncher.Common;
+using XIVLauncher.Common.Dalamud;
+using XIVLauncher.Common.Game.Patch.Acquisition;
+using XIVLauncher.Common.PlatformAbstractions;
+using XIVLauncher.Common.Windows;
+using XIVLauncher.Core.Compatibility;
+using XIVLauncher.Core.Components.LoadingPage;
+using XIVLauncher.Core.Configuration;
+using XIVLauncher.Core.Configuration.Linux;
+using XIVLauncher.Core.Configuration.Parsers;
+
+namespace XIVLauncher.Core;
+
+class Program
+{
+    private static Sdl2Window window;
+    private static CommandList cl;
+    private static GraphicsDevice gd;
+    private static ImGuiBindings bindings;
+
+    public static GraphicsDevice GraphicsDevice => gd;
+    public static ImGuiBindings ImGuiBindings => bindings;
+    public static ILauncherConfig Config { get; private set; }
+    public static CommonSettings CommonSettings => new(Config);
+    public static ISteam? Steam { get; private set; }
+    public static DalamudUpdater DalamudUpdater { get; private set; }
+    public static DalamudOverlayInfoProxy DalamudLoadInfo { get; private set; }
+    public static CompatibilityTools CompatibilityTools { get; private set; }
+
+    private static readonly Vector3 clearColor = new(0.1f, 0.1f, 0.1f);
+    private static bool showImGuiDemoWindow = true;
+
+    private static LauncherApp launcherApp;
+    private static Storage storage;
+
+    private const string APP_NAME = "xlcore";
+
+    private static uint invalidationFrames = 0;
+    private static Vector2 lastMousePosition;
+
+    private static bool lastFrameTextInput = false;
+    public static bool DoAutoSoftwareKbd { get; set; } = true;
+
+    public static void Invalidate(uint frames = 100)
+    {
+        invalidationFrames = frames;
+    }
+
+    private static void SetupLogging()
+    {
+        Log.Logger = new LoggerConfiguration()
+                     .WriteTo.Async(a =>
+                         a.File(Path.Combine(storage.GetFolder("logs").FullName, "launcher.log")))
+#if DEBUG
+                     .WriteTo.Console()
+                     .WriteTo.Debug()
+                     .MinimumLevel.Verbose()
+#else
+                         .MinimumLevel.Information()
+#endif
+                     .CreateLogger();
+    }
+
+    private static void LoadConfig(Storage storage)
+    {
+        Config = new ConfigurationBuilder<ILauncherConfig>()
+                 .UseCommandLineArgs()
+                 .UseIniFile(storage.GetFile("launcher.ini").FullName)
+                 .UseTypeParser(new DirectoryInfoParser())
+                 .UseTypeParser(new AddonListParser())
+                 .Build();
+
+        if (string.IsNullOrEmpty(Config.AcceptLanguage))
+        {
+            Config.AcceptLanguage = Util.GenerateAcceptLanguage();
+        }
+
+        Config.GamePath ??= storage.GetFolder("ffxiv");
+        Config.ClientLanguage ??= ClientLanguage.English;
+        Config.DpiAwareness ??= DpiAwareness.Unaware;
+        Config.IsAutologin ??= false;
+
+        Config.IsDx11 ??= true;
+        Config.IsEncryptArgs ??= true;
+        Config.IsFt ??= false;
+        Config.IsOtpServer ??= false;
+
+        Config.PatchPath ??= storage.GetFolder("patch");
+        Config.PatchAcquisitionMethod ??= AcquisitionMethod.Aria;
+
+        Config.DalamudEnabled ??= true;
+        Config.DalamudLoadMethod = !OperatingSystem.IsWindows() ? DalamudLoadMethod.DllInject : DalamudLoadMethod.EntryPoint;
+
+        Config.GlobalScale ??= 1.0f;
+
+        Config.LinuxStartupType ??= LinuxStartupType.Managed;
+        Config.LinuxStartCommandLine ??= "wine %COMMAND%";
+        Config.WineDebugVars = string.Empty;
+    }
+
+    public const int STEAM_APP_ID = 39210;
+
+    private static void Main(string[] args)
+    {
+        storage = new Storage(APP_NAME);
+        SetupLogging();
+        LoadConfig(storage);
+
+        try
+        {
+            Steam = new WindowsSteam();
+            Steam.Initialize(STEAM_APP_ID);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "steam init fail");
+        }
+
+        DalamudLoadInfo = new DalamudOverlayInfoProxy();
+        DalamudUpdater = new DalamudUpdater(storage.GetFolder("dalamud"), storage.GetFolder("runtime"), storage.GetFolder("dalamudAssets"), null)
+        {
+            Overlay = DalamudLoadInfo
+        };
+        DalamudUpdater.Run();
+
+        CompatibilityTools = new CompatibilityTools(storage);
+
+        Log.Debug("Creating veldrid devices...");
+
+#if DEBUG
+        var version = AppUtil.GetGitHash();
+#else
+        var version = AppUtil.GetAssemblyVersion();
+#endif
+
+        // Create window, GraphicsDevice, and all resources necessary for the demo.
+        VeldridStartup.CreateWindowAndGraphicsDevice(
+            new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, $"XIVLauncher {version}"),
+            new GraphicsDeviceOptions(false, null, true, ResourceBindingModel.Improved, true, true),
+            out window,
+            out gd);
+
+        window.Resized += () =>
+        {
+            gd.MainSwapchain.Resize((uint)window.Width, (uint)window.Height);
+            bindings.WindowResized(window.Width, window.Height);
+            Invalidate();
+        };
+        cl = gd.ResourceFactory.CreateCommandList();
+        Log.Debug("Veldrid OK!");
+
+        bindings = new ImGuiBindings(gd, gd.MainSwapchain.Framebuffer.OutputDescription, window.Width, window.Height);
+        Log.Debug("ImGui OK!");
+
+        StyleModelV1.DalamudStandard.Apply();
+        ImGui.GetIO().FontGlobalScale = Config.GlobalScale ?? 1.0f;
+
+        launcherApp = new LauncherApp(storage);
+
+        Invalidate(20);
+
+        // Main application loop
+        while (window.Exists)
+        {
+            Thread.Sleep(50);
+
+            InputSnapshot snapshot = window.PumpEvents();
+
+            if (!window.Exists)
+                break;
+
+            var overlayNeedsPresent = false;
+
+            if (Steam != null && Steam.IsValid)
+                overlayNeedsPresent = Steam.BOverlayNeedsPresent;
+
+            if (!snapshot.KeyEvents.Any() && !snapshot.MouseEvents.Any() && !snapshot.KeyCharPresses.Any() && invalidationFrames == 0 && lastMousePosition == snapshot.MousePosition
+                && !overlayNeedsPresent)
+            {
+                continue;
+            }
+
+            if (invalidationFrames == 0)
+            {
+                invalidationFrames = 10;
+            }
+
+            if (invalidationFrames > 0)
+            {
+                invalidationFrames--;
+            }
+
+            lastMousePosition = snapshot.MousePosition;
+
+            bindings.Update(1f / 60f, snapshot);
+
+            launcherApp.Draw();
+
+            var wantTextInput = ImGui.GetIO().WantTextInput;
+
+            if (wantTextInput && !lastFrameTextInput && DoAutoSoftwareKbd && Steam.IsRunningOnSteamDeck())
+            {
+                Steam.ShowFloatingGamepadTextInput(ISteam.EFloatingGamepadTextInputMode.EnterDismisses, 0, 0, 100, 100);
+                Log.Verbose("Show kbd");
+                lastFrameTextInput = true;
+            }
+            else if (wantTextInput)
+            {
+                lastFrameTextInput = true;
+            }
+            else
+            {
+                lastFrameTextInput = false;
+            }
+
+            cl.Begin();
+            cl.SetFramebuffer(gd.MainSwapchain.Framebuffer);
+            cl.ClearColorTarget(0, new RgbaFloat(clearColor.X, clearColor.Y, clearColor.Z, 1f));
+            bindings.Render(gd, cl);
+            cl.End();
+            gd.SubmitCommands(cl);
+            gd.SwapBuffers(gd.MainSwapchain);
+        }
+
+        // Clean up Veldrid resources
+        gd.WaitForIdle();
+        bindings.Dispose();
+        cl.Dispose();
+        gd.Dispose();
+    }
+}
