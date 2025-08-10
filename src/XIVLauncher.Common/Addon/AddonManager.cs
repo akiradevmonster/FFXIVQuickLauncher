5362c42943861383fb0afb399ac2f747c2e1b909
Akira Yamamoto <devmonster2417@gmail.com>
2025-08-10 23:52:56 +0900

    create runaddon

--- src/XIVLauncher.Common/Addon/AddonManager.cs
+++ src/XIVLauncher.Common/Addon/AddonManager.cs
@@ -70,6 +70,38 @@ namespace XIVLauncher.Common.Addon
                 _runningAddons = null;
             }
 
+            public void RunAddons(int gamePid, List<IAddon> addonEntries)
+        {
+            if (_runningAddons != null)
+                throw new Exception("Addons still running?");
+
+            _runningAddons = new List<Tuple<IAddon, Thread, CancellationTokenSource>>();
+
+            foreach (var addonEntry in addonEntries)
+            {
+                addonEntry.Setup(gamePid);
+
+                if (addonEntry is IPersistentAddon persistentAddon)
+                {
+                    Log.Information("Starting PersistentAddon {0}", persistentAddon.Name);
+                    var cancellationTokenSource = new CancellationTokenSource();
+
+                    var addonThread = new Thread(persistentAddon.DoWork);
+                    addonThread.Start(cancellationTokenSource.Token);
+
+                    _runningAddons.Add(new Tuple<IAddon, Thread, CancellationTokenSource>(persistentAddon, addonThread, cancellationTokenSource));
+                }
+
+                if (addonEntry is IRunnableAddon runnableAddon)
+                {
+                    Log.Information("Starting RunnableAddon {0}", runnableAddon.Name);
+                    runnableAddon.Run();
+                }
+
+                if (addonEntry is INotifyAddonAfterClose notifiedAddon)
+                    _runningAddons.Add(new Tuple<IAddon, Thread, CancellationTokenSource>(notifiedAddon, null, null));
+            }
+
             IsRunning = false;
         }
     }
