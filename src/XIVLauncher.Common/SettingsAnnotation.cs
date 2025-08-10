e53a8db1a252513d89b557b978bd1b4817f05648
Akira Yamamoto <devmonster2417@gmail.com>
2025-08-10 23:26:16 +0900

    change friendly name

--- src/XIVLauncher.Common/SettingsAnnotation.cs
+++ src/XIVLauncher.Common/SettingsAnnotation.cs
@@ -5,13 +5,10 @@ namespace XIVLauncher.Common;
 [AttributeUsage(AttributeTargets.Field)]
 public class SettingsDescriptionAttribute : Attribute
 {
-    public string FriendlyName { get; set; }
-
     public string Description { get; set; }
 
     public SettingsDescriptionAttribute(string friendlyName, string description)
     {
-        this.FriendlyName = friendlyName;
         this.Description = description;
     }
 }
